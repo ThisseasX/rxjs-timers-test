831cfa1a15249ada6692e03c932ce03fbbcd5fa4
Thisseas X <thisseasx@yahoo.com>
2020-03-04 20:18:18 +0200

    Convert timer to epic on the state stream

--- src/store/timer/epics.js
+++ src/store/timer/epics.js
@@ -1,25 +1,32 @@
-import { BehaviorSubject, interval, empty } from 'rxjs';
-import { mapTo, scan, tap, switchMap, takeWhile } from 'rxjs/operators';
+import { BehaviorSubject, interval, empty, of, from } from 'rxjs';
+import {
+  mapTo,
+  scan,
+  tap,
+  switchMap,
+  takeWhile,
+  mergeMap,
+  mergeMapTo,
+  map,
+} from 'rxjs/operators';
 import { flow } from 'lodash/fp';
 // import { playerReducer, playerActions, playerInitialState } from './player-reducer';
 import { bindActionCreators } from 'redux';
 import { SPEED, INITIALLY_PAUSED } from '/config';
 import { combineEpics, ofType } from 'redux-observable';
+import { subtractTime, addPlaytime, changePlayer } from '/store/timer';
+import { get } from 'lodash/fp';
+import store from '../../store';
 
-const awesomeEpic = () => {
-  // const interval$ = interval(SPEED).pipe(mapTo(SPEED));
-  // const pause$ = useRef(new BehaviorSubject(INITIALLY_PAUSED));
-  // const sub = pause$.current
-  //   .pipe(
-  //     tap(changeButtonText),
-  //     switchMap(paused => (paused ? empty() : interval$)),
-  //     scan((remaining, i) => remaining - i, remainingTime),
-  //     takeWhile(remaining => remaining >= 0),
-  //     tap(subtractTime),
-  //     tap(addPlaytime),
-  //   )
-  //   .subscribe();
-  return empty();
+const interval$ = interval(SPEED).pipe(mapTo(SPEED));
+
+const awesomeEpic = (action$, state$) => {
+  return state$.pipe(
+    switchMap(({ timer: { paused, remainingTime } }) =>
+      paused ? empty() : interval$.pipe(takeWhile(() => remainingTime > 0)),
+    ),
+    mergeMapTo([subtractTime(), addPlaytime()]),
+  );
 };
 
 export default combineEpics(awesomeEpic);
